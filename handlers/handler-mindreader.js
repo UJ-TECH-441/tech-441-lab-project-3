20ad11b7ef910c26aaba77415d7bf2466c5e4567
Josh Guardino <guardino@bu.edu>
2024-01-28 11:09:29 -0500

    Updates

--- handlers/handler-mindreader.js
+++ handlers/handler-mindreader.js
@@ -6,15 +6,15 @@ module.exports.makePredictions = () =>
 	[
 		// Create some horribly wrong predictions
 		{
-			prediction: 'Your name is',
+			prefix: 'Your name is',
 			value: faker.person.fullName(),
 		},
 		{
-			prediction: 'You live in',
+			prefix: 'You live in',
 			value: `${faker.location.city()}, ${faker.location.state({abbreviated: true})}`,
 		},
 		{
-			prediction: 'You work for',
+			prefix: 'You work for',
 			value: faker.company.name()
 		}
 	];
