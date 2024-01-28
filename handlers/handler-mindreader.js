9cca3db755755693904ca376646489a043adfc66
Josh Guardino <guardino@bu.edu>
2024-01-27 23:51:37 -0500

    Updates

--- handlers/handler-mindreader.js
+++ handlers/handler-mindreader.js
@@ -1,10 +1,6 @@
 // The faker-js module creates fake, random data
 const { faker } = require('@faker-js/faker');
 
-// Keep a running total of these counts
-let votes = 0;
-let totalStars = 0;
-
 // The read() function will be called by the index.js route
 module.exports.makePredictions = () =>
 	[
